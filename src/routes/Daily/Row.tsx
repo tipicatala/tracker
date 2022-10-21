18f8719be02eb77d267c2ba3aa8243aaa35d8438
Daria Platonova <plat-dasha@mail.ru>
2022-10-21 12:27:09 +0300

    added drag and drop for activity daily list

---
+++ src/routes/Daily/Row.tsx
@@ -0,0 +1,92 @@
+import React, { ReactComponentElement, ReactHTMLElement, useRef } from "react";
+import clsx from "clsx";
+import { useDrag, useDrop } from 'react-dnd'
+
+import s from "./style.module.scss";
+
+interface IProps {
+  el: string,
+  setActivities: React.Dispatch<React.SetStateAction<string[]>>,
+  activities: string[],
+  index: number,
+  moveRow: (dragIndex: any, hoverIndex: any) => void
+}
+
+function Row({ el, setActivities, activities, index, moveRow }: IProps) {
+  const ref = useRef<HTMLDivElement>(null)
+  const handleRowClick = (el: string) => setActivities((prev) => [...prev, el]);
+
+  const [{ handlerId }, drop] = useDrop({
+    accept: "Row",
+    collect(monitor) {
+      return {
+        handlerId: monitor.getHandlerId(),
+      }
+    },
+    hover(item, monitor) {
+      if (!ref.current) {
+        return
+      }
+      const dragIndex = (item as { index: number}).index
+      const hoverIndex = index
+      // Don't replace items with themselves
+      if (dragIndex === hoverIndex) {
+        return
+      }
+      // Determine rectangle on screen
+      const hoverBoundingRect = ref.current?.getBoundingClientRect()
+      // Get vertical middle
+      const hoverMiddleY =
+        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
+      // Determine mouse position
+      const clientOffset = monitor.getClientOffset() as { y:number }
+      // Get pixels to the top
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top
+      // Only perform the move when the mouse has crossed half of the items height
+      // When dragging downwards, only move when the cursor is below 50%
+      // When dragging upwards, only move when the cursor is above 50%
+      // Dragging downwards
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return
+      }
+      // Dragging upwards
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return
+      }
+      // Time to actually perform the action
+      moveRow(dragIndex, hoverIndex);
+      // Note: we're mutating the monitor item here!
+      // Generally it's better to avoid mutations,
+      // but it's good here for the sake of performance
+      // to avoid expensive index searches.
+      (item as { index: number}).index = hoverIndex
+    },
+  })
+
+  const [{ isDragging }, drag] = useDrag({
+    type: "Row",
+    item: () => {
+      return { el, index }
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  })
+
+  const opacity = isDragging ? 0 : 1
+  drag(drop(ref))
+
+  return (
+    <div
+      ref={ref}
+      style={{ opacity }}
+      data-handler-id={handlerId}
+      className={clsx(s.row, activities.includes(el) && s.row_selected)}
+      onClick={() => handleRowClick(el)}
+    >
+      {el}
+    </div>
+  );
+}
+
+export default Row;
--- src/routes/Daily/index.tsx
+++ src/routes/Daily/index.tsx
@@ -1,45 +1,75 @@
-import { useReducer, useState } from "react";
+import { useReducer, useState, useRef, useEffect, useCallback } from "react";
 import useSWR from "swr";
-import clsx from "clsx"
+import clsx from "clsx";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import update from 'immutability-helper'
 
-import { fetcher } from "../../utils";
 import { useUserStore } from "../../stores/userStore";
-import { setProbableActivities } from "../../api/user";
+import { setProbableActivities, getUser } from "../../api/user";
 
 import s from "./style.module.scss";
 
 import Button from "../../components/Button";
+import Row from "./Row";
 
 function Daily() {
+  const dragItem = useRef<number | null>(null);
+  const dragOverItem = useRef<number | null>(null);
+
   const [activities, setActivities] = useState<string[]>([]);
+  const [data, setData] = useState([""]);
   const id = useUserStore((state) => state.id);
 
-  const { data } = useSWR(`${import.meta.env.VITE_URL}/user/${id}`, fetcher);
+  useEffect(() => {
+    (async function () {
+      const data = await getUser(id);
+      setData(data.probable_activities);
+    })();
+  }, []);
 
-  console.log(data)
+  const handleButtonClick = async () =>
+    await setProbableActivities(activities, id);
 
-  const handleRowClick = (el: string) => setActivities(prev => [...prev, el]);
-  const handleButtonClick = async () => await setProbableActivities(activities, id);
+  const moveRow = useCallback((dragIndex:number, hoverIndex:number) => {
+      setData((prevCards) =>
+        update(prevCards, {
+          $splice: [
+            [dragIndex, 1],
+            [hoverIndex, 0, prevCards[dragIndex]],
+          ],
+        }),
+      )
+  }, [])
 
   return (
-    <div className={s.root}>
-      <div className={s.card}>
-        <div className={s.title}>
-          Choose or add things that you did today
-        </div>
-        <div>
-          {data &&
-            data.probable_activities.map((el: string) => (
-              <div key={el} className={clsx(s.row, activities.includes(el) && s.row_selected)} onClick={() => handleRowClick(el)}>
-                {el}
-              </div>
-            ))}
-        </div>
-        <div className={s.button}>
-        {/* <Button text="Continue" handleClick={handleButtonClick}/> */}
+    <DndProvider backend={HTML5Backend}>
+      <div className={s.root}>
+        <div className={s.card}>
+          <div className={s.title}>
+            <div>Choose or add things that you did today,</div>
+            <div>select time frame</div>
+            and locate in order of completion each task
+          </div>
+          <div>
+            {data &&
+              data.map((el: string, index: number) => (
+                <Row
+                  index={index}
+                  key={el}
+                  el={el}
+                  setActivities={setActivities}
+                  activities={activities}
+                  moveRow={moveRow}
+                />
+              ))}
+          </div>
+          <div className={s.button}>
+            {/* <Button text="Continue" handleClick={handleButtonClick}/> */}
+          </div>
         </div>
       </div>
-    </div>
+    </DndProvider>
   );
 }
 
